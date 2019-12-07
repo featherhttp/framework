657908b93881bcec9069926b1d90b893696a75d7
Martin Solev <martin.solev@tripx.se>
2019-12-07 13:06:21 +0100

    Want sum samples?

---
+++ Samples/SignalRClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalRClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:3000/chat")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            connection.On<string>("Send", (message) =>
+            {
+                Console.WriteLine($"Recieved: '{message}'");
+            });
+
+            await connection.StartAsync();
+
+            while (true)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(3));
+                await connection.InvokeAsync("Send", "Hello World!");
+            }
+
+        }
+    }
+}
---
+++ Samples/SignalRServer/Program.cs
@@ -0,0 +1,33 @@
+﻿using FeatherHttp;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.SignalR;
+
+class Chat : Hub
+{
+    public Task Send(string message) => Clients.All.SendAsync("Send", message);
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebApplicationHost.CreateDefaultBuilder(args);
+
+        builder.UseUrls("http://localhost:3000");
+
+        builder.Services.AddSignalR();
+
+        var host = builder.Build();
+
+        var app = host.ApplicationBuilder;
+
+        var routes = app.UseRouter();
+
+        routes.MapHub<Chat>("/chat");
+
+        await host.RunAsync();
+    }
+}
