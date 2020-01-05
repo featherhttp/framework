a4fee756c9c19c1b0ba284b7809bdae63dfc75fa
James MacIvor <jmacivor@steemer.com>
2020-01-04 20:14:54 -0500

    added template project resources

---
+++ template/content/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var builder = WebApplicationHost.CreateDefaultBuilder(args);
+
+        var app = builder.Build();
+
+        app.MapGet("/", async context =>
+        {
+            await context.Response.WriteAsync("Hello World!");
+        });
+
+        await app.RunAsync();
+    }
+}
