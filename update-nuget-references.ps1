5ccae90ba2e71d85fc69fab87ec6a99611370f79
James MacIvor <jmacivor@steemer.com>
2020-01-06 20:25:15 -0500

    added log messages to job flow

--- update-nuget-references.ps1
+++ update-nuget-references.ps1
@@ -11,11 +11,13 @@ if ($version -ne "Default") {
     $nuspecXml = [xml](Get-Content ./template/FeatherHttpTemplate.nuspec)
     $nuspecXml.selectNodes("//version") | ForEach-Object {
       $_."#text" = $version.ToString()
+      Write-Host "Updated nuspec"
     }
     $nuspecXml.save("./template/FeatherHttpTemplate.nuspec")
     $csprojXml = [xml](Get-Content ./template/content/featherhttp-template.csproj)
     $csprojXml.selectNodes("//PackageReference") | ForEach-Object {
       $_.Version = $version.ToString()
+      Write-Host "Updated csproj"
     }
     $csprojXml.save("./template/content/featherhttp-template.csproj")
 }
