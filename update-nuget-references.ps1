8220663039a29a2563ddbb95698fb8db8afc6897
James <RobotOptimist@users.noreply.github.com>
2020-01-11 13:02:44 -0500

    Ci changes (#2)
    * added ci changes
    
    * rearranged order of run and shell for step
    
    * commented out nuget steps
    
    * fixed powershell syntax errors
    
    * fixed nbgv needs called like ./nbgv in gh actions
    
    * testing feedz.io workflow
    
    * fixed yaml indentation error
    
    * temporarily disable ci conditionals to test nuget deployment
    
    * removed conditional from nuget setup
    
    * moved from simple string replace to xml modification
    
    * fixed powershell syntax error
    
    * added job to commit change to repository
    
    * downloading dotnet 3.1 SDK in second job
    
    * now to try something crazy...
    
    * maybe maybe maybe
    
    * added powershell script
    
    * attempting to add changes differently
    
    * added back dotnet tool install
    
    * add .store/ to .gitignore
    
    * update nuget packages from different job
    
    * added log messages to job flow
    
    * fixed issue with nbgv
    
    * attempt to fix issue with github-push-action
    
    * Updated Nuget Version
    
    * added null check to $version
    
    * added needs to run jobs sequentially
    
    * modify fetch depth to resolve nbgv error
    
    * adding nbgv installation to own step
    
    * reordered jobs
    
    * changed checkout version
    
    * iunno been drinkin lets do this sh
    
    * Updated Nuget Version
    
    * test template build
    
    * Updated Nuget Version
    
    * additional changes to ci to test template build
    
    * added change to allow nuget for testing
    
    * Updated Nuget Version
    
    * added test build to end of ci
    
    * Updated Nuget Version
    
    * removed 3rd checkout
    
    * Updated Nuget Version
    
    * reintroduced condtionals on jobs, macivor to davidfowl
    
    * attempt to retrieve dll version using powershell
    
    * attempt to retrieve version with powershell take 2
    
    * attempt to set version to github actions variable
    
    * attempt to set version to github actions variable take 2
    
    * attempt to set version to github actions variable take 3
    
    * attempt 4
    
    * attempt 5
    
    * attempt 6
    
    * attempt 7
    
    * attempt 8
    
    * Updated Nuget Version
    
    * update template nuspec and csproj

--- update-nuget-references.ps1
+++ update-nuget-references.ps1
@@ -1,24 +1,16 @@
-$version = "Default"
-if (Get-Command "nbgv.exe" -ErrorAction SilentlyContinue) {
-    #if testing locally you will need to install nbgv
-    #dotnet tools install -g nbgv
-    $version = nbgv get-version -v NugetPackageVersion;
-}
-elseif (Get-Command "./nbgv.exe" -ErrorAction SilentlyContinue) {
-    $version = ./nbgv get-version -v NugetPackageVersion;    
-}
+$version = $(Get-Content NUGET_VERSION)
 if (!$version) { $version = "Default" }
 if ($version -ne "Default") {
     $nuspecXml = [xml](Get-Content ./template/FeatherHttpTemplate.nuspec)
     $nuspecXml.selectNodes("//version") | ForEach-Object {
       $_."#text" = $version.ToString()
-      Write-Host "Updated nuspec"
+      Write-Host "Updated template nuspec"
     }
     $nuspecXml.save("./template/FeatherHttpTemplate.nuspec")
     $csprojXml = [xml](Get-Content ./template/content/featherhttp-template.csproj)
     $csprojXml.selectNodes("//PackageReference") | ForEach-Object {
       $_.Version = $version.ToString()
-      Write-Host "Updated csproj"
+      Write-Host "Updated template csproj"
     }
     $csprojXml.save("./template/content/featherhttp-template.csproj")
 }
