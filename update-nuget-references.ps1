52698409e91929c7875ec2fdc93dfdc1608814df
James MacIvor <jmacivor@steemer.com>
2020-01-11 12:46:29 -0500

    attempt 8

--- update-nuget-references.ps1
+++ update-nuget-references.ps1
@@ -4,13 +4,13 @@ if ($version -ne "Default") {
     $nuspecXml = [xml](Get-Content ./template/FeatherHttpTemplate.nuspec)
     $nuspecXml.selectNodes("//version") | ForEach-Object {
       $_."#text" = $version.ToString()
-      Write-Host "Updated nuspec"
+      Write-Host "Updated template nuspec"
     }
     $nuspecXml.save("./template/FeatherHttpTemplate.nuspec")
     $csprojXml = [xml](Get-Content ./template/content/featherhttp-template.csproj)
     $csprojXml.selectNodes("//PackageReference") | ForEach-Object {
       $_.Version = $version.ToString()
-      Write-Host "Updated csproj"
+      Write-Host "Updated template csproj"
     }
     $csprojXml.save("./template/content/featherhttp-template.csproj")
 }
