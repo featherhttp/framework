15041377d6c8ed9064aea0975bce1c42a208c208
David Fowler <davidfowl@gmail.com>
2020-01-22 04:26:45 -0800

    More hacks! Populate initial configuration object properly

---
+++ src/FeatherHttp/ConfigurationHostBuilder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Microsoft.Extensions.Configuration
+{
+    // This exists solely to bootstrap the configuration
+    internal class ConfigurationHostBuilder : IHostBuilder
+    {
+        public IDictionary<object, object> Properties { get; } = new Dictionary<object, object>();
+        private readonly HostBuilderContext _context;
+        private readonly Configuration _configuration;
+
+        public ConfigurationHostBuilder(Configuration configuration, IWebHostEnvironment webHostEnvironment)
+        {
+            _configuration = configuration;
+            _context = new HostBuilderContext(Properties)
+            {
+                Configuration = configuration,
+                HostingEnvironment = webHostEnvironment
+            };
+        }
+
+        public IHost Build()
+        {
+            return null;
+        }
+
+        public IHostBuilder ConfigureAppConfiguration(Action<HostBuilderContext, IConfigurationBuilder> configureDelegate)
+        {
+            configureDelegate(_context, _configuration);
+
+            return this;
+        }
+
+        public IHostBuilder ConfigureContainer<TContainerBuilder>(Action<HostBuilderContext, TContainerBuilder> configureDelegate)
+        {
+            return this;
+        }
+
+        public IHostBuilder ConfigureHostConfiguration(Action<IConfigurationBuilder> configureDelegate)
+        {
+            configureDelegate(_configuration);
+            return this;
+        }
+
+        public IHostBuilder ConfigureServices(Action<HostBuilderContext, IServiceCollection> configureDelegate)
+        {
+            return this;
+        }
+
+        public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(IServiceProviderFactory<TContainerBuilder> factory)
+        {
+            return this;
+        }
+
+        public IHostBuilder UseServiceProviderFactory<TContainerBuilder>(Func<HostBuilderContext, IServiceProviderFactory<TContainerBuilder>> factory)
+        {
+            return this;
+        }
+    }
+}
