54a986cd81c018b485f03577b867447061c5a5e2
James <RobotOptimist@users.noreply.github.com>
2020-02-01 17:10:54 -0500

    Dotnet template ci (#21)
    * added folders to .gitignore
    
    * added nuget.config with reference to f.feedz.io/davidfowl
    
    * added nuget.config with reference to f.feedz.io/davidfowl
    
    * added Program.cs with hello world example
    
    * added FeatherHttpContent.csproj with project references
    
    * added template.json
    
    * added version.json for nuget template
    
    * added FeatherHttpProject.csproj
    
    * added ci changes
    
    * removed branch requirement from checkout
    
    * cleaning up template csproj
    
    * wrong csproj was referenced in template.json
    
    * removed Program.cs and nuget.config as content items from csproj
    
    * removed hard coded version in template
    
    * fixed ci to string replace featherhttp  version in FeatherHttpContent.csproj
    
    * fixed typo in path on update version step
    
    * using legitimate build variable in FeatherHttpContent.csproj
    
    * rolledback to actions checkout@v1
    
    * removed template/Project/version.json as it is unnecessary
    
    * fixed typo in template.json

---
+++ template/Content/Program.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace FeatherHttp.Templates.HelloWorld 
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var app = builder.Build();
+
+            app.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+
+            await app.RunAsync();
+        }
+    }
+}
