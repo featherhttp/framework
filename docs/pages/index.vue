dac2f2ff86ce023765e0313dd40a6efea7111206
James MacIvor <jmacivor@steemer.com>
2020-02-05 08:07:24 -0500

    add feather http brief to home page

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -1,8 +1,13 @@
-<template>
-  <div class="lg:ml-10 p-10 pt-0">    
-    <div class="lg:flex lg:justify-center w-full">            
-    </div>
-  </div>
+<template lang="md">
+## Feather HTTP
+
+[![feedz.io](https://img.shields.io/badge/endpoint.svg?url=https%3A%2F%2Ff.feedz.io%2Fdavidfowl%2Ffeatherhttp%2Fshield%2FFeatherHttp%2Flatest&label=FeatherHttp)](https://f.feedz.io/davidfowl/featherhttp/packages/FeatherHttp/latest/download)
+
+A lightweight low ceremony APIs for ASP.NET Core applications.
+
+- Built on the same primitives as ASP.NET Core
+- Optimized for building HTTP APIs quickly
+- Take advantage of existing ASP.NET Core middleware and frameworks
 </template>
 
 <script>
